de0a70df029a5bfbbee6d87794b6b3726a47aa0e
Mosopefoluwa Eniserije <eniserijemosope@gmail.com>
2026-06-20 21:52:11 +0100

    code update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePage()
+        ),
       ),
-    ),
-  );
+    ); 
 }
 
 class DicePage extends StatefulWidget {
@@ -23,44 +21,17 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-
-  void changeDiceFace() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Image.asset(
-                'images/dice$leftDiceNumber.png',
-              ),
-              onPressed: () {
-                changeDiceFace();
-              },
-            ),
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png')
           ),
-          //Get students to create the second die as a challenge
-          Expanded(
-            child: FlatButton(
-              child: Image.asset(
-                'images/dice$rightDiceNumber.png',
-              ),
-              onPressed: () {
-                changeDiceFace();
-              },
-            ),
+        Expanded(
+          child: Image.asset('images/dice1.png')
           ),
-        ],
-      ),
+      ]
     );
   }
 }
