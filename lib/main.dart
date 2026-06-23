1ba193993fc1cda2f00dd9321ba980ac9d546e78
Mosopefoluwa Eniserije <eniserijemosope@gmail.com>
2026-06-23 11:54:03 +0100

    hm

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,8 @@ void main() {
     ); 
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Row(
